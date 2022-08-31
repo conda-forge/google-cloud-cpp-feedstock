b62462d3312adfc5523ea16d832dcfb62d28498c
H. Vetinari <h.vetinari@gmx.com>
2022-08-31 17:51:58 +0200

    Revert "Keep windows static"
    This reverts commit 440ce9c994ca5f66a38a02902faf17dc4fbd78e2.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,7 @@ cmake -G "Ninja" ^
     -S . -B build ^
     -DGOOGLE_CLOUD_CPP_ENABLE=__ga_libraries__ ^
     -DBUILD_TESTING=OFF ^
-    -DBUILD_SHARED_LIBS=OFF ^
+    -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
