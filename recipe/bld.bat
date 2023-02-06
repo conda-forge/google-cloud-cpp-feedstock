46d25a0ae451d504a26412570aed7ab352b1dbaf
Carlos O'Ryan <coryan@google.com>
2023-02-06 14:20:23 +0000

    feat: build all libraries in the package
    By default `google-cloud-cpp` only builds the most commonly used
    libraries. When building from source that can save some time, but for
    package managers this leaves out many important features.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,14 @@
 @echo on
 setlocal EnableDelayedExpansion
 
-mkdir build
-cd build
-
 @REM CMake does not like paths with \ characters
 set LIBRARY_PREFIX="%LIBRARY_PREFIX:\=/%"
 set BUILD_PREFIX="%BUILD_PREFIX:\=/%"
 set SRC_DIR="%SRC_DIR:\=/%"
 
 cmake -G "Ninja" ^
+    -S . -B build ^
+    -DGOOGLE_CLOUD_CPP_ENABLE=__ga_libraries__ ^
     -DBUILD_TESTING=OFF ^
     -DBUILD_SHARED_LIBS=OFF ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -18,9 +17,8 @@ cmake -G "Ninja" ^
     -DCMAKE_MODULE_PATH="%LIBRARY_PREFIX%/lib/cmake" ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DGOOGLE_CLOUD_CPP_ENABLE_EXAMPLES=OFF ^
-    -DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF ^
-    ..
+    -DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF
 if %ERRORLEVEL% neq 0 exit 1
 
-cmake --build . --config Release
+cmake --build build --config Release
 if %ERRORLEVEL% neq 0 exit 1
