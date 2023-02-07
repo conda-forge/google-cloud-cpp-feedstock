b31c647fa3d80681f2f0d56ad6e6554e2014e611
Carlos O'Ryan <coryan@google.com>
2023-02-07 01:35:26 +0000

    feat: build all libraries in the package (#125)
    By default `google-cloud-cpp` only builds the most commonly used
    libraries. When building from source that can save some time, but for
    package managers this leaves out many important features.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,14 @@
 
 set -euo pipefail
 
-mkdir build_cmake
-pushd build_cmake
-
 export OPENSSL_ROOT_DIR=$PREFIX
 
 # Overwrite location of protoc plugin to support cross-compilation
-sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=${BUILD_PREFIX}/bin/grpc_cpp_plugin;g" ../cmake/CompileProtos.cmake
+sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=${BUILD_PREFIX}/bin/grpc_cpp_plugin;g" cmake/CompileProtos.cmake
 
 cmake ${CMAKE_ARGS} \
-    -GNinja \
+    -GNinja -S . -B build_cmake \
+    -DGOOGLE_CLOUD_CPP_ENABLE=__ga_libraries__ \
     -DBUILD_TESTING=OFF \
     -DBUILD_SHARED_LIBS=ON \
     -DOPENSSL_ROOT_DIR=$PREFIX \
@@ -20,8 +18,6 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc \
-    -DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF \
-    ..
+    -DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF
 
-ninja
-popd
+cmake --build build_cmake
