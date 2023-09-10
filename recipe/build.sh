c2bfdf96737ec1bb93650182428af65ac94a43a6
Carlos O'Ryan <coryan@google.com>
2023-09-10 21:00:32 +0000

    WIP - use subpackage for 'full' build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,28 +9,17 @@ if [[ "${target_platform}" == osx-* ]]; then
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-cmake ${CMAKE_ARGS} \
-    -GNinja -S . -B .build-core \
-    -DGOOGLE_CLOUD_CPP_ENABLE=bigtable,iam,pubsub,storage,spanner \
-    -DBUILD_TESTING=OFF \
-    -DBUILD_SHARED_LIBS=ON \
-    -DOPENSSL_ROOT_DIR=$PREFIX \
-    -DCMAKE_BUILD_TYPE=release \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc \
-    -DGOOGLE_CLOUD_CPP_GRPC_PLUGIN_EXECUTABLE=$BUILD_PREFIX/bin/grpc_cpp_plugin \
-    -DGOOGLE_CLOUD_CPP_ENABLE_WERROR=OFF
-
-cmake --build .build-core
-
-cmake --install .build-core --component google_cloud_cpp_development
+FEATURES=bigtable,iam,pubsub,storage,spanner
+USE_INSTALLED_COMMON=OFF
+if [[ "$PKG_NAME" == "google-cloud-cpp-full" ]]; then
+  FEATURES=__ga_libraries__,-bigtable,-iam,-pubsub,-storage,-spanner
+  USE_INSTALLED_COMMON=ON
+fi
 
 cmake ${CMAKE_ARGS} \
     -GNinja -S . -B build_cmake \
-    -DGOOGLE_CLOUD_CPP_ENABLE=__ga_libraries__,-bigtable,-iam,-pubsub,-storage,-spanner \
-    -DGOOGLE_CLOUD_CPP_USE_INSTALLED_COMMON=ON \
+    -DGOOGLE_CLOUD_CPP_ENABLE=$FEATURES \
+    -DGOOGLE_CLOUD_CPP_USE_INSTALLED_COMMON=$USE_INSTALLED_COMMON \
     -DBUILD_TESTING=OFF \
     -DBUILD_SHARED_LIBS=ON \
     -DOPENSSL_ROOT_DIR=$PREFIX \
