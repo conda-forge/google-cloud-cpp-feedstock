22500bc9d8b767a9d74b7d927d9e523a382660f8
Carlos O'Ryan <coryan@google.com>
2023-09-14 12:20:21 +0000

    WIP - fix install script for Windows

--- recipe/build-libgoogle-cloud.bat
+++ recipe/build-libgoogle-cloud.bat
@@ -9,14 +9,14 @@ set SRC_DIR="%SRC_DIR:\=/%"
 if [%PKG_NAME%] == [libgoogle-cloud] (
   @rem cmake --install build_cmake --component google_cloud_cpp_runtime
   if %ERRORLEVEL% neq 0 exit 1
-) else if [%PKG_NAME] == [google-cloud-cpp] (
+) else if [%PKG_NAME%] == [google-cloud-cpp] (
   @rem cmake --install build_cmake --component google_cloud_cpp_development
   cmake --install build
   if %ERRORLEVEL% neq 0 exit 1
-) else if [%PKG_NAME] == [libgoogle-cloud-full] (
+) else if [%PKG_NAME%] == [libgoogle-cloud-full] (
   @rem cmake --install build_full --component google_cloud_cpp_runtime
   if %ERRORLEVEL% neq 0 exit 1
-) else if [%PKG_NAME] == [google-cloud-cpp-full] (
+) else if [%PKG_NAME%] == [google-cloud-cpp-full] (
   @rem cmake --install build_full --component google_cloud_cpp_development
   cmake --install build_full
   if %ERRORLEVEL% neq 0 exit 1
